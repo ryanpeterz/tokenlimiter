d3b8a9a6dfd3a11651c16f24765e6e908bd9b6d1
ryanpeterz <ryanpeterz@users.noreply.github.com>
2022-12-10 20:58:39 -0800

    Create test_tokenlimiter.py

---
+++ tests/test_tokenlimiter.py
@@ -0,0 +1,23 @@
+from tokenlimiter import TokenLimiter
+
+# tested by ryanpeterz
+def _run_tests(quota: int = 300, period: float = 1):
+    tokenlimiter = TokenLimiter(quota, period,
+                                callback=lambda t: print(f"Block@{time.monotonic()}"))
+
+    start = time.monotonic()
+    gas_spent = [0]
+
+    def ex(n: int):
+        print(f"Op costs {n} gas.")
+        gas_spent[0] = gas_spent[0] + n
+        gpm = gas_spent[0] / (time.monotonic() - start)
+        assert gpm < (quota / period)
+        print(f"Current gpm: {gpm}")
+
+    with tokenlimiter as burn:
+        for _ in range(10):
+            burn(40)
+            ex(40)
+
+if __name__ == "__main__": _run_tests()
